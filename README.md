8b4c45245f49b931fbce94e322902d43215bf1aa
klai <root@kali.null>
2018-12-14 00:45:14 +0800

    lunarjoll fixed

---
+++ README.md
@@ -0,0 +1,9 @@
+#ABOUT
+It is a fork from [linyha](https://github.com/linyha/tieba.gitI)
+I update origin to python3, use requests instead of urllib.
+
+#indenpendice  
+pip3 install requests
+
+#HOW TO  
+python3 tieba.py 
--- tieba.py
+++ tieba.py
@@ -1,7 +1,20 @@
 # -*- coding:utf-8 -*-
-import urllib
-import urllib2
-import re
+#import urllib.request
+#import urllib.error
+import re,os,traceback
+import requests
+from io import BytesIO
+#from PIL import Image
+#from multiprocessing import Pool,cpu_count,freeze_support
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from selenium.webdriver import Firefox
+#from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.support import expected_conditions as expected
+#from selenium.webdriver.support.wait import WebDriverWait
+#
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
 
 class Tool:
     #替换工具
@@ -36,15 +49,39 @@ class BDTB:
 
     def getpage(self, pageNum):
         #获取贴吧页面
+        url = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
+        headers={'use-agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",'Referer':'http://manhua.dmzj.com/tags/s.shtml'}
         try:
-            url = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
-            request = urllib2.Request(url)
-            response = urllib2.urlopen(request)
+            response = requests.get(url,headers=headers).text
+        except ConnectionError:
+            print('tieba connect fail')
+            traceback.print_exc()
+            raise ConnectionError
+            #response = urllib.request.urlopen(url)
+            #request = urllib.request.urlopen(url)
+            #response = urllib.urlopen(request)
             #print response.read()
-            return response.read()
-        except urllib2.URLError, e:
-            if hasattr(e, "reason"):
-                print u"连接百度贴吧失败，错误原因", e.reason
+        return response
+
+#        except urllib.error.URLError as e:
+##        except urllib3.URLError as e:
+#            if hasattr(e, "reason"):
+#                print (u"连接百度贴吧失败，错误原因", e.reason)
+
+#    def headless_get(self,pageNum):
+#        url = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
+#        chrome_options = webdriver.ChromeOptions()
+#        prefs = {"profile.managed_default_content_settings.images": 2}
+#        chrome_options.add_experimental_option("prefs", prefs)
+#        driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver",chrome_options=chrome_options)
+#        driver.get(url)
+#        text=driver.page_source
+#        print(text)
+#        contents = self.getContent(text)
+#        driver.quit()
+#        return contents
+#
+
 
     def getTitle(self, page):
         #获取标题
@@ -57,9 +94,11 @@ class BDTB:
             return None
 
     def getPageNum(self, page):
-        #获取页数
-        #page = self.getpage()
+            #获取页数
+            #page = self.getpage()
         pattern = re.compile('<span class="red">(.*?)</span>', re.S)
+        #pattern='<span class="red">(.*?)</span>'
+        #print(page)
         result = re.search(pattern, page)
         if result:
             return result.group(1).strip()
@@ -98,25 +137,31 @@ class BDTB:
         title = self.getTitle(indexPage)
         self.setFileTitle(title)
         if pageNum == None:
-            print "URL无效，请重试"
+            print ("URL无效，请重试")
             return
         try:
-            print u"该帖子共有" + str(pageNum) + u"页"
+            print (u"该帖子共有" + str(pageNum) + u"页")
             for i in range(1, int(pageNum)+1):
-                print "正在写入第" + str(i) + "页数据"
-                page = self.getpage(i)
+                print ("正在写入第" + str(i) + "页数据")
+#                page = self.headless_get(i)
+                if i == 1:
+                    page=indexPage
+                else:
+                    page=self.getpage(i)
                 contents = self.getContent(page)
                 self.writeData(contents)
-        except IOError, e:
-            print "写入异常，原因"+ e.message
+#                self.writeData(page)
+        except IOError as e :
+            print ("写入异常，原因"+ e.message)
         finally:
-            print "写入任务完成"
+            print ("写入任务完成")
+            self.file.close()
 
 
-print "请输入帖子代号"
-baseURL = 'http://tieba.baidu.com/p/' + str(raw_input(u'http://tieba.baidu.com/p/'))
-seeLZ = raw_input("是否只看楼主，是请输入1\n")
-floorTag = raw_input("是否写入楼层信息，是输入1\n")
+print ("请输入帖子代号")
+baseURL = 'http://tieba.baidu.com/p/' + str(input(u'http://tieba.baidu.com/p/'))
+seeLZ = input("是否只看楼主，是请输入1\n")
+floorTag = input("是否写入楼层信息，是输入1\n")
 bdtb = BDTB(baseURL,seeLZ,floorTag)
 bdtb.start()
 
